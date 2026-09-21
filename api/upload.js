279ba357e303b4edbcd09238c11bb709a64c698d
MARCOS SIQUEIRA DOS SANTOS <106713226+msiqueira96@users.noreply.github.com>
2026-09-21 18:50:48 -0300

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     }
     const buffer = Buffer.concat(chunks);
 
-    const n8nUrl = new URL('https://webhook.labzratz.tech/webhook/f24a3071-8062-4589-a3bc-7ddbd95e63da');
+    const n8nUrl = new URL('https://webhook.labzratz.tech/webhook/upload-document');
 
     const options = {
       hostname: n8nUrl.hostname,
